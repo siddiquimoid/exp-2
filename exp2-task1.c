68a029863dc9b8f245f854232f8720f1e0d7b9d8
Siddiqui Abdul Moid - 58 <siddiquimoid@eng.rizvi.edu.in>
2025-11-16 23:29:38 +0530

    Update exp2-task1.c

--- exp2-task1.c
+++ exp2-task1.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-int main()
-{
-    
-    int num;
-    printf("Enter a number = ",num);
-    scanf("%i",&num);
-    (num %2 == 0)? printf("The entered number is even")
-                :  printf("The entered number is odd");
-    (num %2 == 0)? num++ : num--;
 
-}
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    
+    if (num % 2 == 0) {
+        printf("%d is even.\n", num);
+    } else {
+        printf("%d is odd.\n", num);
+    }

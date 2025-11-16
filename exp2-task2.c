0a315b30ac05927faabcb2e8fb1854e45e6f2a13
Siddiqui Abdul Moid - 58 <siddiquimoid@eng.rizvi.edu.in>
2025-11-16 23:41:23 +0530

    Update exp2-task2.c

--- exp2-task2.c
+++ exp2-task2.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+
 int main()
 {
-    
-    int num1,num2,i,sum=0;
+    int num1, num2, i, sum = 0;
 
-    printf("Enter a number = ");
-    scanf("%i",&num1);
-    printf("\nEnter a number = ");
-    scanf("%i",&num2);
-    
-    for(i=num1; i<=num2; i++){
-        if(i %2 !=0){
-            sum==i;
+    printf("Enter first number = ");
+    scanf("%i", &num1);
+    printf("Enter second number = ");
+    scanf("%i", &num2);
+
+    for (i = num1; i <= num2; i++) {
+        if (i % 2 != 0) {
+            sum += i; 
         }
     }
-    printf("\nSum of odd number is = %i",sum);
+
+    printf("\nSum of odd numbers between %i and %i is = %i\n", num1, num2, sum);
     return 0;
 }
